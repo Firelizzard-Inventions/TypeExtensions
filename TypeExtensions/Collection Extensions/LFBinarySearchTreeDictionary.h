5d15cac736b9c9f3df0617c8b06af5ed994421d5
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-11-21 00:00:20 -0500

    Adding a binary search tree based dictionary

---
+++ TypeExtensions/Collection Extensions/LFBinarySearchTreeDictionary.h
@@ -0,0 +1,13 @@
+//
+//  LFBinarySearchTreeDictionary.h
+//  TypeExtensions
+//
+//  Created by Ethan Reesor on 11/18/13.
+//  Copyright (c) 2013 Lens Flare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LFBinarySearchTreeDictionary : NSDictionary
+
+@end
---
+++ TypeExtensions/Collection Extensions/LFBinarySearchTreeDictionary.m
@@ -0,0 +1,13 @@
+//
+//  LFBinarySearchTreeDictionary.m
+//  TypeExtensions
+//
+//  Created by Ethan Reesor on 11/18/13.
+//  Copyright (c) 2013 Lens Flare. All rights reserved.
+//
+
+#import "LFBinarySearchTreeDictionary.h"
+
+@implementation LFBinarySearchTreeDictionary
+
+@end
