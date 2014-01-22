503c54d7718d55f71cadcef63129aec0c2380e89
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-01-21 21:45:08 -0500

    Moving proxy stuff to branch (2/2)

---
+++ TypeExtensions/Other Extensions/TXZeroingWeakProxy.h
@@ -0,0 +1,13 @@
+//
+//  TXZeroingWeakProxy.h
+//  TypeExtensions
+//
+//  Created by Ethan Reesor on 1/21/14.
+//  Copyright (c) 2014 Lens Flare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TXZeroingWeakProxy : NSProxy
+
+@end
